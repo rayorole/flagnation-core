26754781c9672715cd4a91495ffdc65c7f2c5505
Ray Orolé <orole.ray@protonmail.com>
2021-09-19 22:04:35 +0200

    Added buyFlag function and secured other functions

--- src/contracts/Contract.sol
+++ src/contracts/Contract.sol
@@ -8,9 +8,17 @@ contract Contract is ERC721Full {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721Full("Flagnation", "FNT") public {}
+    address owner;
+
+    mapping(uint256 => uint256) public flagPrice;
+    mapping(uint256 => bool) public flagForSale;
+
+    constructor() ERC721Full("Flagnation", "FNT") public {
+        owner = msg.sender;
+    }
 
     function createFlag(address holder, string memory tokenURI) public returns (uint256) {
+        require(owner == msg.sender, 'Only owner can mint new flags');
         require(_tokenIds.current() <= 10000, 'Only a max of 10,000 tokens are allowed');
         _tokenIds.increment();
 
@@ -21,20 +29,58 @@ contract Contract is ERC721Full {
         return newItemId;
     }
 
-    function transferFlag(address from, address to, uint256 tokenId /* bytes memory _data */) public {
-        transferFrom(from, to, tokenId);
+    function transferFlag(address from, address to, uint256 _tokenId /* bytes memory _data */) public {
+        require(msg.sender == ownerOf(_tokenId), 'msg.sender must be flag owner');
+        transferFrom(from, to, _tokenId);
     }
 
-    function approveFlag(address to, uint256 tokenId) public {
-        approve(to, tokenId);
+
+    function setForSale(uint256 _tokenId, uint256 _tokenPrice) external {
+        address flagOwner = ownerOf(_tokenId);
+
+        require(flagOwner == msg.sender, 'Only owner can sell flag');
+
+        // allowance[_tokenId] = address(this);
+        flagForSale[_tokenId] = true;
+        // set the sale price etc
+        flagPrice[_tokenId] = _tokenPrice;
+
+        emit Approval(flagOwner, address(this), _tokenId);
     }
 
-    function burnFlag(uint256 tokenId) public {
-        _burn(tokenId);
-        _tokenIds.decrement();
+    function buyFlag(uint256 _tokenId) external payable {
+        address buyer = msg.sender;
+        uint payedPrice = msg.value;
+        uint256 salePrice = flagPrice[_tokenId];
+        bool isForSale = flagForSale[_tokenId];
+
+        // require(isValidToken(_tokenId), 'Token must be valid');
+        // require(getApproved(_tokenId) == address(this));
+        require (payedPrice >= salePrice, 'Buyer must pay enough');
+        require(isForSale == true, 'Flag must be for sale');
+
+        // pay the seller
+        transferFrom(ownerOf(_tokenId), buyer, _tokenId);
+        // remove token from tokensForSale
+        flagForSale[_tokenId] = false;
+    }
+
+    function getPriceFlag(uint256 _tokenId) public view returns(uint256) {
+        return flagPrice[_tokenId];
     }
 
     function getMsgSender() public view returns(address) {
         return msg.sender;
+    }
+
+    function setOwner(address _newOwner) public returns(bool) {
+        require(msg.sender == owner, 'Function caller must be owner');
+        owner = _newOwner;
+
+        return(true);
+    }
+
+    function getOwner() public view returns(address) {
+        return owner;
     }
 }
--- test/Contract.test.js
+++ test/Contract.test.js
@@ -1,83 +1,73 @@
-const { assert } = require('chai')
+const { assert } = require("chai");
 
-const Contract = artifacts.require('./Contract.sol')
+const Contract = artifacts.require("./Contract.sol");
 
-require('chai')
-  .use(require('chai-as-promised'))
-  .should()
+require("chai").use(require("chai-as-promised")).should();
 
-contract('Flagnation', (accounts) => {
-  let contract
+contract("Flagnation", (accounts) => {
+  let contract;
 
   before(async () => {
-    contract = await Contract.deployed()
-  })
+    contract = await Contract.deployed();
+  });
 
-  describe('deployment', async () => {
-    it('deploys successfully', async () => {
-      const address = contract.address
-      assert.notEqual(address, 0x0)
-      assert.notEqual(address, '')
-      assert.notEqual(address, null)
-      assert.notEqual(address, undefined)
-    })
+  describe("deployment", async () => {
+    it("deploys successfully", async () => {
+      const address = contract.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
 
-    it('has a name', async () => {
-      const name = await contract.name()
-      assert.equal(name, 'Flagnation')
-    })
+    it("has a name", async () => {
+      const name = await contract.name();
+      assert.equal(name, "Flagnation");
+    });
 
-    it('has a symbol', async () => {
-      const symbol = await contract.symbol()
-      assert.equal(symbol, 'FNT')
-    })
+    it("has a symbol", async () => {
+      const symbol = await contract.symbol();
+      assert.equal(symbol, "FNT");
+    });
+  });
 
-  })
+  describe("minting function", async () => {
+    it("first mint", async () => {
+      const mint = await contract.createFlag(
+        "0x7d3Cb8dFFB9E33A98FC6e7e39b0e7d83a4CC6D22",
+        "https://some.json.document"
+      );
+      const totalSupply = await contract.totalSupply();
 
-  
+      assert.equal(mint.receipt.status, true || "true");
+    });
+  });
 
-  describe('minting function', async () => {
-    it('first mint', async () => {
-      const mint = await contract.createFlag('0x3c0898974de2472ef405882851a89568254e0364', 'https://some.json.document')
-      const totalSupply = await contract.totalSupply()
+  describe("setOwner function", async () => {
+    it("sets new owner correctly", async () => {
+      const adress = "0xA68FeCC8971a9218558ddb798218F79442Dd44cE";
+      await contract.setOwner(adress);
+      let res = await contract.getOwner();
 
-      assert.equal(mint.receipt.status, true || 'true')
-    })
+      assert.equal(res.toString(), adress);
+    });
+  });
 
-    it('second mint', async () => {
-      const mint = await contract.createFlag('0x3c0898974de2472ef405882851a89568254e0364', 'https://some.json.document')
-      const totalSupply = await contract.totalSupply()
+  describe("openzeppelin defaults", async () => {
+    it("check balance of adress", async () => {
+      let balance = await contract.balanceOf(
+        "0x7d3Cb8dFFB9E33A98FC6e7e39b0e7d83a4CC6D22"
+      );
+      assert.equal(balance.toString(), "1");
+    });
 
-      
-      assert.equal(mint.receipt.status, true || 'true')
-    })
-  })
+    it("check owner of token", async () => {
+      await contract.ownerOf(1);
+    });
 
-  describe('burning function', async () => {
-    it('burn first token', async () => {
-      const burn = await contract.burnFlag(1)
-      const totalSupply = await contract.totalSupply()
-
-      assert.equal(totalSupply.toString(), '1')
-    })
-  })
-
-  describe('openzeppelin defaults', async () => {
-    it('check balance of adress', async () => {
-      let balance = await contract.balanceOf('0x3c0898974de2472ef405882851a89568254e0364')
-      assert.equal(balance.toString(), '1')
-    })
-
-    it('check owner of token', async () => {
-      await contract.ownerOf(2)
-    })
-
-    it('check uri of token', async () => {
-      let uri = await contract.tokenURI(2)
-      assert.equal(uri, 'https://some.json.document')
-    })
-  })
-
-  describe('burnFlag function', async () => {
-  })
-})
+    it("check uri of token", async () => {
+      let uri = await contract.tokenURI(1);
+      assert.equal(uri, "https://some.json.document");
+    });
+  });
+});
